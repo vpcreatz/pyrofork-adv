e30991709bf500f79024855df22e07fb7bd9b937
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2024-07-29 20:46:54 +0530

    .

--- pyrogram/methods/chats/__init__.py
+++ pyrogram/methods/chats/__init__.py
@@ -45,6 +45,7 @@ from .get_chat_member import GetChatMember
 from .get_chat_members import GetChatMembers
 from .get_chat_members_count import GetChatMembersCount
 from .get_chat_online_count import GetChatOnlineCount
+from .get_chat_pending_members_count import GetChatPendingMembersCount
 from .get_dialogs import GetDialogs
 from .get_dialogs_count import GetDialogsCount
 from .get_folders import GetFolders
